b79e92eba0a8edc63b135eadcaf65ccba4d4cdfc
shokes <oyatioshoke@yahoo.com>
2022-04-18 04:37:32 -0700

    new question

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 //The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
 
-const speedCoverter = function (km) {
-  const convert = km * 27.7778;
-  console.log(convert);
-  console.log(Math.floor(convert));
-};
-speedCoverter(1.08);
-speedCoverter(2);
-speedCoverter(2.5544);
+// const speedCoverter = function (km) {
+//   const convert = km * 27.7778;
 
-speedCoverter(56.4);
+//  // console.log(convert);
 
-speedCoverter(58.6);
+// console.log(Math.floor(convert));
+// };
+// speedCoverter(1.08);
+// speedCoverter(2);
+// speedCoverter(2.5544);
+
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// const mutliarray = function (values) {
+//   const newarray = values.reduce((acc, val) => acc * val);
+
+//   console.log(newarray);
+// };
+
+// mutliarray([1, 2, 3, 4]);
